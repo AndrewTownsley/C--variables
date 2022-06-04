851502dd494f4d4194f3dc6166629e054b3ef031
Andrew TOwnsley <atownsley86@gmail.com>
2022-06-04 10:19:35 -0600

    Basic C# variables

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+           
+
+
+
+
+
+
+/*            const float Pi = 3.14f;
+*/        /*    Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            Console.WriteLine(" {0} {1}", float.MinValue, float.MaxValue);*/
+         /*   byte number = 8;
+            var id = 9;
+            int count = 10;
+            var cartCount = 10;
+            float totalPrice = 20.95f;
+            var cartTotal = 32.99f;
+            char character = 'k';
+            var charFirst = 'L';
+            string firstName = "Andrew";
+            var lastName = "Wiamton";
+            bool isOpen = false;
+            var isSelected = true;
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(character);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isOpen);*/
+        }
+    }
+}
